83ec9994cf0c2a8b3cf46e04f5f730c50583d5ec
咕叽咕叽 <1465678596@qq.com>
2018-12-04 13:00:48 +0800

    Update ManagerReg.aspx.cs

--- Project/ManagerReg.aspx.cs
+++ Project/ManagerReg.aspx.cs
@@ -8,7 +8,7 @@ using System.Web.UI.WebControls;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace Project
+namespace Project 
 {
     public partial class ManagerReg : System.Web.UI.Page
     {
